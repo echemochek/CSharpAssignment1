df81242c171f89ebe3b277f96cf3a46427582917
echemochek <echemochek@gmail.com>
2021-09-07 09:22:59 -0400

    Edited the HalvesAreAlike method

--- Program.cs
+++ Program.cs
@@ -8,14 +8,40 @@ namespace CSharpAssignment1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Question 1
+            Console.WriteLine("Q1 : Enter the string:");
+            string s = Console.ReadLine();
+            bool pos = HalvesAreAlike(s);
+            if (pos)
+            {
+                Console.WriteLine("Both Halfs of the string are alike");
+            }
+            else
+            {
+                Console.WriteLine("Both Halfs of the string are not alike");
+            }
+
+            Console.WriteLine();
+
         }
+        
         private static bool HalvesAreAlike(string s)
         {
+            string vowels = "aeiouAEIOU";
+            int a = 0, b = 0;
             try
             {
                 // write your code here
-                return false;
+                for (int i = 0; i <= s.Length / 2; i++)
+                {
+                    if (vowels.Contains(s[i])){
+                    a += 1;
+                    }
+                    if (vowels.Contains(s[-i-1])) {
+                    b += 1;
+                    }
+                }
+                return a==b;
             }
             catch (Exception)
             {
